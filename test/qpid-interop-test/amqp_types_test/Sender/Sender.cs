ba8423ca2c2bb4ae3ad741763d4be994447486e6
Chuck Rolke <crolke@redhat.com>
2016-12-02 15:56:47 -0500

    interop dev

---
+++ test/qpid-interop-test/amqp_types_test/Sender/Sender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amqp;
+using System.Runtime.Serialization.Json;
+
+namespace Sender
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * --- main ---
+             * Args: 1: Broker address (ip-addr:port)
+             *       2: Queue name
+             *       3: AMQP type
+             *       4: Test value(s) as JSON string
+             */
+        }
+    }
+}
