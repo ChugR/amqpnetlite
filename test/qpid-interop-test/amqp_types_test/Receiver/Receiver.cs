41114e02a4bf0664541b0d2781ecc973ec6d2216
Chuck Rolke <crolke@redhat.com>
2016-12-07 16:00:26 -0500

    Cwip

--- test/qpid-interop-test/amqp_types_test/Receiver/Receiver.cs
+++ test/qpid-interop-test/amqp_types_test/Receiver/Receiver.cs
@@ -1,23 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
+using System.Text;
 using System.Threading;
 using Amqp;
 using Amqp.Framing;
 using Amqp.Types;
 //using System.Runtime.Serialization.Json;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
 
 
 namespace Qpidit
 {
+    /// <summary>
+    /// Class to convert incoming Message into QPIDIT particulars
+    /// </summary>
     class AnalyzedMessage
     {
         private Message _message;
         private string _liteType;
         private string _qType;
         private string _qString;
+
 
         public override string ToString()
         {
@@ -31,7 +34,7 @@ namespace Qpidit
         /// <param name="input">The byte array.</param>
         /// <param name="suppressLeading0s">Flag controls suppression of leading zeros.</param>
         /// <returns>Hexadecimal string</returns>
-        public string BytesRevdToString(byte[] input, bool suppressLeading0s = false)
+        public string BytesReversedToString(byte[] input, bool suppressLeading0s = false)
         {
             string result = "";
             for (int i = input.Length -1; i >= 0; i--)
@@ -46,6 +49,21 @@ namespace Qpidit
         }
 
 
+        /// <summary>
+        /// Return the input string surrounded with double quotes
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string Quoted(string input)
+        {
+            return "\"" + input + "\"";
+        }
+
+
+        /// <summary>
+        /// Constructor that analyzes the message
+        /// </summary>
+        /// <param name="message"></param>
         public AnalyzedMessage(Message message)
         {
             _message = message;
@@ -53,84 +71,127 @@ namespace Qpidit
             object body = message.Body;
             if (body == null)
             {
-                Console.WriteLine("AnalyzedMessage body type : null");
+                //Console.WriteLine("AnalyzedMessage body type : null");
                 _liteType = "null";
                 _qType = "null";
-                _qString = "null";
+                _qString = Quoted("null");
             }
             else
             {
                 _liteType = body.GetType().Name;
-                Console.WriteLine("AnalyzedMessage body type : {0}", _liteType);
+                //Console.WriteLine("AnalyzedMessage body type : {0}", _liteType);
                 switch (_liteType) {
                     case "Boolean":
                         _qType = "boolean";
-                        _qString = ((Boolean)body ? "True" : "False");
+                        _qString = Quoted(((Boolean)body ? "True" : "False"));
                         break;
                     case "Byte":
                         _qType = "ubyte";
-                        _qString = String.Format("0x{0:x}", (Byte)body);
+                        _qString = Quoted(String.Format("0x{0:x}", (Byte)body));
                         break;
                     case "UInt16":
                         _qType = "ushort";
-                        _qString = String.Format("0x{0:x}", (UInt16)body);
+                        _qString = Quoted(String.Format("0x{0:x}", (UInt16)body));
                         break;
                     case "UInt32":
                         _qType = "uint";
-                        _qString = String.Format("0x{0:x}", (UInt32)body);
+                        _qString = Quoted(String.Format("0x{0:x}", (UInt32)body));
                         break;
                     case "UInt64":
                         _qType = "ulong";
-                        _qString = String.Format("0x{0:x}", (UInt64)body);
+                        _qString = Quoted(String.Format("0x{0:x}", (UInt64)body));
                         break;
                     case "SByte":
                         _qType = "byte";
-                        _qString = String.Format("0x{0:x}", (SByte)body);
+                        _qString = Quoted(String.Format("0x{0:x}", (SByte)body));
                         break;
                     case "Int16":
-                        _qType = "byte";
-                        _qString = String.Format("0x{0:x}", (Int16)body);
+                        _qType = "short";
+                        _qString = Quoted(String.Format("0x{0:x}", (Int16)body));
                         break;
                     case "Int32":
-                        _qType = "byte";
-                        _qString = String.Format("0x{0:x}", (Int32)body);
+                        _qType = "int";
+                        _qString = Quoted(String.Format("0x{0:x}", (Int32)body));
                         break;
                     case "Int64":
-                        _qType = "byte";
-                        _qString = String.Format("0x{0:x}", (Int64)body);
+                        _qType = "long";
+                        _qString = Quoted(String.Format("0x{0:x}", (Int64)body));
                         break;
                     case "Single":
                         byte[] sbytes = BitConverter.GetBytes((Single)body);
                         _qType = "float";
-                        _qString = "0x" + BytesRevdToString(sbytes);
+                        _qString = Quoted("0x" + BytesReversedToString(sbytes));
                         break;
                     case "Double":
                         byte[] dbytes = BitConverter.GetBytes((Double)body);
                         _qType = "double";
-                        _qString = "0x" + BytesRevdToString(dbytes);
+                        _qString = "\"0x" + BytesReversedToString(dbytes) + "\"";
                         break;
                     case "DateTime":
+                        // epochTicks is the number of 100uSec ticks between 01/01/0001
+                        // and 01/01/1970. Used to adjust between DateTime and unix epoch.
                         const long epochTicks = 621355968000000000;
                         byte[] dtbytes = BitConverter.GetBytes(
                             (((DateTime)body).Ticks - epochTicks) / TimeSpan.TicksPerMillisecond);
                         _qType = "timestamp";
-                        _qString = "0x" + BytesRevdToString(dtbytes, true);
+                        _qString = Quoted("0x" + BytesReversedToString(dtbytes, true));
                         break;
                     case "Guid":
+                        _qType = "uuid";
+                        _qString = Quoted(body.ToString());
                         break;
                     case "Byte[]":
+                        _qType = "binary";
+                        byte[] binstr = (byte[])body;
+                        StringBuilder builder = new StringBuilder();
+                        foreach (byte b in binstr)
+                            if (b >= 32 && b <= 127)
+                                builder.Append((char)b);
+                            else
+                                builder.Append(String.Format("\\{0:x2}", b));
+                        _qString = Quoted(builder.ToString());
                         break;
                     case "String":
+                        _qType = "string";
+                        _qString = Quoted(body.ToString());
                         break;
                     case "Symbol":
+                        _qType = "symbol";
+                        _qString = Quoted(body.ToString());
                         break;
                     case "List":
+                        _qType = "list";
+                        List list = (List)body;
+                        string result = "[";
+                        int li = 0;
+                        foreach (string item in list)
+                        {
+                            if (li++ > 0) result += ", ";
+                            result += Quoted(item);
+                        }
+                        result += "]";
+                        _qString = result;
                         break;
                     case "Map":
+                        _qType = "map";
+                        Map map = (Map)body;
+                        string mresult = "{";
+                        int mi = 0;
+                        foreach (var key in map.Keys)
+                        {
+                            if (mi++ > 0) mresult += ", ";
+                            mresult += Quoted(key.ToString());
+                            mresult += ":";
+                            mresult += Quoted(map[key].ToString());
+                        }
+                        mresult += "}";
+                        _qString = mresult;
                         break;
+                    default:
+                        string errString = String.Format("Unknown AMQP type: {0}", _liteType);
+                        throw new ApplicationException(errString);
                 }
             }
-
         }
 
         public string LiteType {  get { return _liteType; } }
@@ -166,6 +227,10 @@ namespace Qpidit
         }
 
 
+        /// <summary>
+        /// Function to analyz
+        /// </summary>
+        /// <param name="message"></param>
         public void messageVerify(Message message)
         {
             // Verify that message body is of expected type
@@ -243,7 +308,7 @@ namespace Qpidit
                         _received += 1;
                         // receiverlink.Accept(message); HACK - don't ack so the message hangs around
                         AnalyzedMessage am = new AnalyzedMessage(message);
-                        Console.WriteLine("{0} [\"{1}\"]", am.QpiditType, am);
+                        Console.WriteLine("{0} [{1}]", am.QpiditType, am);
                         _receivedValueList = string.Format("Received {0} of {1} messages", _received, _expected);
                     }
                     else
@@ -281,25 +346,8 @@ namespace Qpidit
 
             int exitCode = 0;
 
-            foreach (string arg in args)
-            {
-                Console.WriteLine(arg);
-            }
-
             try
             {
-                //HACK: serialize objects into string
-                //string instring = "[\"0x0\", \"0x7fffffff\", \"0x80000000\", \"0xffffffff\"]";
-                List<int> listOfInts = new List<int>();
-                listOfInts.Add(0);
-                listOfInts.Add(1);
-                listOfInts.Add(2);
-                listOfInts.Add(-1);
-
-                var result = JsonConvert.SerializeObject(listOfInts);
-                Console.WriteLine("The list became: {0}", result);
-                // End HACK Alert
-
                 // create and run a receiver
                 Receiver receiver = new Qpidit.Receiver(args[0], args[1], args[2], UInt32.Parse(args[3]));
                 receiver.run();
