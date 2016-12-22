97e236b7f1dd653ae5cb261249aea54f0d55e6b8
Chuck Rolke <crolke@redhat.com>
2016-12-22 12:07:26 -0500

    Add copy utils

---
+++ copy_receiver.bat
@@ -0,0 +1,3 @@
+REM Copy the lite Receiver.cs to qpid-interop-test
+
+copy D:\Users\crolke\git\amqpnetlite\test\qpid-interop-test\amqp_types_test\Receiver\Receiver.cs D:\Users\crolke\git\qpid-interop-test\shims\amqpnetlite\src\amqp_types_test\Receiver\Receiver.cs
