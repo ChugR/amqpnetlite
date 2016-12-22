9313ca9bb67d74f16d10302a35dc5c00e9db03ba
Chuck Rolke <crolke@redhat.com>
2016-12-22 12:07:26 -0500

    Add copy utils

---
+++ copy_sender.bat
@@ -0,0 +1,3 @@
+REM Copy the lite Sender.cs to qpid-interop-test
+
+copy D:\Users\crolke\git\amqpnetlite\test\qpid-interop-test\amqp_types_test\Sender\Sender.cs D:\Users\crolke\git\qpid-interop-test\shims\amqpnetlite\src\amqp_types_test\Sender\Sender.cs
