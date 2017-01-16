66a3cd6b5c747e1d64d2cc215eb1e99e2f01a953
Chuck Rolke <crolke@redhat.com>
2017-01-16 17:01:24 -0500

    Add bunches of log statements to narrate AMQP traffic in log

--- TransactionTestProgram/TransactionTestProgram.cs
+++ TransactionTestProgram/TransactionTestProgram.cs
@@ -9,56 +9,73 @@ using Amqp;
 using Amqp.Framing;
 using System.Transactions;
 
-namespace Test.Amqp
+namespace TransactionTestProgram
 {
-    class Program
+    class Tests
     {
-        static void TransactedPosting(Address addr, string target)
+        public void log(string what)
+        {
+            System.Diagnostics.Trace.WriteLine(DateTime.Now.ToString("[hh:mm:ss.fff]") + " " + what);
+            //Console.Error.WriteLine(DateTime.Now.ToString("[hh:mm:ss.fff]") + " " + what);
+        }
+
+        public void TransactedPosting(Address addr, string target)
         {
             string testName = "TransactedPosting";
             int nMsgs = 5;
+            Boolean testpass = true;
 
             Connection connection = new Connection(addr);
             Session session = new Session(connection);
             SenderLink sender = new SenderLink(session, "sender-" + testName, target);
 
             // commit
+            log(testName);
+            log(testName + ": creating transaction scope for Complete set");
+            log(testName);
             using (var ts = new TransactionScope())
             {
                 for (int i = 0; i < nMsgs; i++)
                 {
+                    log("Writing message with id commit" + i);
                     Message message = new Message("test");
                     message.Properties = new Properties() { MessageId = "commit" + i, GroupId = testName };
                     sender.Send(message);
                 }
-
+                log("Transaction scope Complete()");
                 ts.Complete();
             }
 
             // rollback
+            log(testName + ": creating transaction scope for Rollback set");
             using (var ts = new TransactionScope())
             {
                 for (int i = nMsgs; i < nMsgs * 2; i++)
                 {
+                    log("Writing message with id rollback" + i);
                     Message message = new Message("test");
                     message.Properties = new Properties() { MessageId = "rollback" + i, GroupId = testName };
                     sender.Send(message);
                 }
+                log("Exiting transaction scope without calling complete");
             }
 
             // commit
+            log(testName + ": creating txn scope for 2nd Complete set");
             using (var ts = new TransactionScope())
             {
                 for (int i = 0; i < nMsgs; i++)
                 {
+                    log("Writing message with id commit" + i);
                     Message message = new Message("test");
                     message.Properties = new Properties() { MessageId = "commit" + i, GroupId = testName };
                     sender.Send(message);
                 }
-
+                log("Transaction scope Complete()");
                 ts.Complete();
             }
 
+            log("Receiving two blocks of messages");
             ReceiverLink receiver = new ReceiverLink(session, "receiver-" + testName, target);
             for (int i = 0; i < nMsgs * 2; i++)
             {
@@ -68,24 +85,31 @@ namespace Test.Amqp
                 if (!message.Properties.MessageId.StartsWith("commit"))
                 {
                     Console.Error.WriteLine("MessageId does not start with 'commit'");
+                    testpass = false;
                 }
             }
 
+            log(testName + " exiting with status " + (testpass ? "PASS" : "FAIL"));
             connection.Close();
         }
 
-        static void TransactedRetiring(Address addr, string target)
+        public void TransactedRetiring(Address addr, string target)
         {
             string testName = "TransactedRetiring";
             int nMsgs = 10;
+            bool testpass = true;
 
             Connection connection = new Connection(addr);
             Session session = new Session(connection);
             SenderLink sender = new SenderLink(session, "sender-" + testName, target);
 
             // send one extra for validation
+            log(testName);
+            log(testName + " Send N+1 with no transaction scope");
+            log(testName);
             for (int i = 0; i < nMsgs + 1; i++)
             {
+                log("Sending message with Id msg" + i);
                 Message message = new Message("test");
                 message.Properties = new Properties() { MessageId = "msg" + i, GroupId = testName };
                 sender.Send(message);
@@ -93,6 +117,9 @@ namespace Test.Amqp
 
             ReceiverLink receiver = new ReceiverLink(session, "receiver-" + testName, target);
             Message[] messages = new Message[nMsgs];
+            log("");
+            log("Receive N messages but don't accept any");
+            log("");
             for (int i = 0; i < nMsgs; i++)
             {
                 messages[i] = receiver.Receive();
@@ -100,63 +127,82 @@ namespace Test.Amqp
             }
 
             // commit harf
+            log("Create txn scope and accept half the messages");
             using (var ts = new TransactionScope())
             {
                 for (int i = 0; i < nMsgs / 2; i++)
                 {
+                    log("Accepting messageId: " + messages[i].Properties.MessageId);
                     receiver.Accept(messages[i]);
                 }
-
+                log("Txn scope complete");
                 ts.Complete();
             }
 
             // rollback
+            log("");
+            log("Create txn scope and accept other half BUT");
+            log(" do that in a failed txn scope that should roll back.");
+            log("");
             using (var ts = new TransactionScope())
             {
                 for (int i = nMsgs / 2; i < nMsgs; i++)
                 {
+                    log("Accepting messageId: " + messages[i].Properties.MessageId);
                     receiver.Accept(messages[i]);
                 }
+                log("Close txn scope without calling complete");
             }
 
             // after rollback, messages should be still acquired
             {
+                log("Receiving a single message");
                 Message message = receiver.Receive();
                 if (!message.Properties.MessageId.Equals("msg" + nMsgs))
                 {
-                    Console.Error.WriteLine("MessageId does not match sequence");
+                    log("MessageId: " + message.Properties.MessageId +
+                        " does not match expected msg" + nMsgs);
+                    testpass = false;
                 }
+                log("Releasing last message received, the extra one");
                 receiver.Release(message);
             }
 
             // commit
+            log("Creating txn scope to accept 2nd half for real this time");
             using (var ts = new TransactionScope())
             {
                 for (int i = nMsgs / 2; i < nMsgs; i++)
                 {
+                    log("Accepting messageId " + messages[i].Properties.MessageId);
                     receiver.Accept(messages[i]);
                 }
-
+                log("Txn Complete()");
                 ts.Complete();
             }
 
             // only the last message is left
             {
+                log("Receive last message again");
                 Message message = receiver.Receive();
                 if (!message.Properties.MessageId.Equals("msg" + nMsgs))
                 {
-                    Console.Error.WriteLine("MessageId does not match sequence");
+                    log("MessageId: " + message.Properties.MessageId +
+                        " does not match expected msg" + nMsgs);
+                    testpass = false;
                 }
+                log("Acccept last message");
                 receiver.Accept(message);
             }
 
             // at this point, the queue should have zero messages.
             // If there are messages, it is a bug in the broker.
+            log(testName + " exiting with status " + (testpass ? "PASS" : "FAIL"));
 
             connection.Close();
         }
 
-        static void TransactedRetiringAndPosting(Address addr, string target)
+        public void TransactedRetiringAndPosting(Address addr, string target)
         {
             string testName = "TransactedRetiringAndPosting";
             int nMsgs = 10;
@@ -217,22 +263,30 @@ namespace Test.Amqp
 
             connection.Close();
         }
+    }
 
-
-
+    class Program
+    {
         static void Main(string[] args)
         {
             Address address = new Address("amqp://10.19.176.108:5672");
+//            Address address = new Address("amqp://admin:password@10.10.56.155:5672");
+            if (args.Length > 0)
+            {
+                address = new Address(args[0]);
+            }
             string target = "q1";
 
             Connection.DisableServerCertValidation = true;
             // uncomment the following to write frame traces
-            Trace.TraceLevel = TraceLevel.Frame;
+            Trace.TraceLevel = TraceLevel.Frame | TraceLevel.Verbose | TraceLevel.Output;
             Trace.TraceListener = (f, a) => System.Diagnostics.Trace.WriteLine(DateTime.Now.ToString("[hh:mm:ss.fff]") + " " + string.Format(f, a));
 
-            TransactedPosting(address, target);
-            TransactedRetiring(address, target);
-            TransactedRetiringAndPosting(address, target);
+            Tests tests = new TransactionTestProgram.Tests();
+
+            tests.TransactedPosting(address, target);
+            tests.TransactedRetiring(address, target);
+            //TransactedRetiringAndPosting(address, target);
 
         }
     }
