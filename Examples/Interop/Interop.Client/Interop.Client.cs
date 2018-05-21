f28ea25df9fa17f1162ad36279934410374cc8e8
Chuck Rolke <crolke@redhat.com>
2018-05-21 15:11:55 -0400

    Lite-272: example test program

--- Examples/Interop/Interop.Client/Interop.Client.cs
+++ Examples/Interop/Interop.Client/Interop.Client.cs
@@ -1,134 +1,94 @@
-﻿//  ------------------------------------------------------------------------------------
-//  Copyright (c) Microsoft Corporation
-//  All rights reserved. 
-//  
-//  Licensed under the Apache License, Version 2.0 (the ""License""); you may not use this 
-//  file except in compliance with the License. You may obtain a copy of the License at 
-//  http://www.apache.org/licenses/LICENSE-2.0  
-//  
-//  THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-//  EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR 
-//  CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR 
-//  NON-INFRINGEMENT. 
-// 
-//  See the Apache Version 2.0 License for specific language governing permissions and 
-//  limitations under the License.
-//  ------------------------------------------------------------------------------------
+﻿//#define CLIENT_MANAGES_CREDIT
+#define LITE_MANAGES_CREDIT
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
-using Amqp;
 using Amqp.Framing;
+//using Amqp.Extensions;
+using Amqp.Sasl;
 using Amqp.Types;
+using System.Threading.Tasks;
 
-namespace Examples.Interop
+namespace Amqp.Extensions.Examples
 {
-    class Client
+    class Program
     {
-        //
-        // Return message as string.
-        //
-        static String GetContent(Message msg)
+        static void Main(string[] args)
         {
-            object body = msg.Body;
-            return body == null ? null : body.ToString();
-        }
 
-        //
-        // Sample invocation: Interop.Client amqp://guest:guest@localhost:5672 [loopcount]
-        //
-        static int Main(string[] args)
-        {
-            String url = "amqp://guest:guest@localhost:5672";
-            String requestQueueName = "service_queue";
-            int loopcount = 1;
+            Random rnd = new Random();
 
-            if (args.Length > 0)
-                url = args[0];
-            if (args.Length > 1)
-                loopcount = Convert.ToInt32(args[1]);
+            Connection connection = new Connection(new Address("amqp://10.3.116.84:5672"));
+            Session session = new Session(connection);
+            ReceiverLink receiver = new ReceiverLink(session, "receiver", "orders");
 
-            Connection.DisableServerCertValidation = true;
-            // uncomment the following to write frame traces
-            //Trace.TraceLevel = TraceLevel.Frame;
-            //Trace.TraceListener = (l, f, a) => Console.WriteLine(DateTime.Now.ToString("[hh:mm:ss.fff]") + " " + string.Format(f, a));
+            // sleep to make sure the queue is created before we send, we could await the attach frame but this is just a demo.
+            Thread.Sleep(1000);
 
-            Connection connection = null;
-            try
+            SenderLink sender = new SenderLink(session, "sender", "orders");
+            Message message = new Message("a message!");
+            message.Header = new Header();
+            message.Header.Durable = true;
+
+            Console.WriteLine("Sending N messages...");
+            int N = 200;
+            for (var i = 0; i < N; i++)
             {
-                Address address = new Address(url);
-                connection = new Connection(address);
-                Session session = new Session(connection);
-
-                // Sender attaches to fixed request queue name
-                SenderLink sender  = new SenderLink(session, "Interop.Client-sender", requestQueueName);
-
-                // Receiver attaches to dynamic address.
-                // Discover its name when it attaches.
-                String replyTo = "";
-                ManualResetEvent receiverAttached = new ManualResetEvent(false);
-                OnAttached onReceiverAttached = (l, a) =>
-                {
-                    replyTo = ((Source)a.Source).Address;
-                    receiverAttached.Set();
-                };
-
-                // Create receiver and wait for it to attach.
-                ReceiverLink receiver = new ReceiverLink(
-                    session, "Interop.Client-receiver", new Source() { Dynamic = true }, onReceiverAttached);
-                if (receiverAttached.WaitOne(10000))
-                {
-                    // Receiver is attached.
-                    // Send a series of requests, gather and print responses.
-                    String[] requests = new String[] {
-                        "Twas brillig, and the slithy toves",
-                        "Did gire and gymble in the wabe.",
-                        "All mimsy were the borogoves,",
-                        "And the mome raths outgrabe."
-                    };
-
-                    for (int j = 0; j < loopcount; j++)
-                    {
-                        Console.WriteLine("Pass {0}", j);
-                        for (int i = 0; i < requests.Length; i++)
-                        {
-                            Message request = new Message(requests[i]);
-                            request.Properties = new Properties() { MessageId = "request" + i, ReplyTo = replyTo };
-                            sender.Send(request);
-                            Message response = receiver.Receive();
-                            if (null != response)
-                            {
-                                receiver.Accept(response);
-                                Console.WriteLine("Processed request: {0} -> {1}",
-                                    GetContent(request), GetContent(response));
-                            }
-                            else
-                            {
-                                throw new ApplicationException(
-                                    String.Format("Receiver timeout receiving response {0}", i));
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    throw new ApplicationException("Receiver attach timeout");
-                }
-                receiver.Close();
-                sender.Close();
-                session.Close();
-                connection.Close();
-                return 0;
+                sender.Send(message);
             }
-            catch (Exception e)
+            Console.WriteLine(".... Done sending");
+
+            Trace.TraceLevel = TraceLevel.Verbose | TraceLevel.Error |
+            TraceLevel.Frame | TraceLevel.Information | TraceLevel.Warning;
+            Trace.TraceListener = (l, f, o) => Console.WriteLine(DateTime.Now.ToString("[hh:mm:ss.fff]") + " " + string.Format(f, o));
+
+            sender.Close();
+
+#if CLIENT_MANAGES_CREDIT
+            int nInProgress = 0;
+            int msgsRetired = 0;
+            const int creditEveryN = 5;
+            const int credit = 10;
+            int msgsRetired = 0;
+
+            receiver.Start(0, async (r, m) =>
             {
-                Console.Error.WriteLine("Exception {0}.", e);
-                if (null != connection)
+                int depth;
+                int nRetired;
+                depth = Interlocked.Increment(ref nInProgress);
+                double delay = System.Convert.ToDouble(rnd.Next(2, 20));
+                Console.WriteLine("Depth= {0} In receive callback. Starting await of {1} seconds", depth, delay);
+                await Task.Delay(TimeSpan.FromSeconds(delay));
+                r.Accept(m);
+                depth = Interlocked.Decrement(ref nInProgress);
+                Console.WriteLine("Depth= {0} Exiting.  Accepting message", depth);
+                nRetired = Interlocked.Increment(ref msgsRetired);
+                if (nRetired % creditEveryN == 0)
                 {
-                    connection.Close();
+                    receiver.SetCredit(credit - depth, false);
                 }
-            }
-            return 1;
+            });
+            receiver.SetCredit(credit, false);
+#endif
+
+#if LITE_MANAGES_CREDIT
+            // never ack... just delay so we can see messages "in-flight" for 60 seconds.
+            // we should only see 10 received... we see all 1000 instead.
+            int nIn = 0;
+            int nDone = 0;
+            receiver.Start(10, async (r, m) =>
+            {
+                nIn++;
+                Console.WriteLine("nIn = {0}, nDone = {1}, InFlight = {2} In receive callback. Starting await...", nIn, nDone, nIn-nDone);
+                double delay = System.Convert.ToDouble(rnd.Next(2, 20));
+                await Task.Delay(TimeSpan.FromSeconds(delay));
+                r.Accept(m);
+                nDone++;
+                Console.WriteLine("nIn = {0}, nDone = {1}, InFlight = {2} ... Exiting Task.Delay", nIn, nDone, nIn - nDone);
+            });
+#endif
+            Thread.Sleep(3000000);
         }
     }
 }
